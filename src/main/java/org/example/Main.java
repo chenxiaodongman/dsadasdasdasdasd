c84a2fbf1ac6bff7b7255a4c2d08040e5bb3760c
chenx <chenxiaoodongdong8866@gamil.com>
2023-12-18 21:10:58 +0800

    dsdsa

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+
+/**
+ * The type Main.
+ */
+public class Main {
+
+
+    /**
+     //     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("是!");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+    public static void mains(String[] args) {
+        System.out.print("是!");
+        for (int i = 1; i <= 5; i++) {
+            for (int ii = 1; i <= 5; i++){
+//                System.out.println("i = " + i+(((()))));
+            }
+            System.out.println("i = " + i);
+//            for
+            System.out.printf("dsa");
+        }
+    }
+}
+
+
